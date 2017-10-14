6e80ac68e57b7617184bbc417541837585daed27
Unknown <jalorn7@gmail.com>
2017-10-13 19:01:49 -0700

    Pause Menu Refinement
    Starting to test audio pausing, no real progress yet

--- Halfway Home/Assets/OpenPauseMenu.cs
+++ Halfway Home/Assets/OpenPauseMenu.cs
@@ -1,31 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AK;
 
 public class OpenPauseMenu : MonoBehaviour
 {
-    bool Paused;
+   public PauseMenu MenuObject;
+   bool Paused;
+   public AkAmbient HackySoundThing;
 
 	// Use this for initialization
 	void Start ()
     {
+        Space.Connect<DefaultEvent>(Events.Pause, OnPause);
         Space.Connect<DefaultEvent>(Events.UnPause, OnUnPause);
-	}
+	  }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Paused)
-            return;
-		if(Input.GetKeyDown(KeyCode.Escape))
+		  if(Input.GetButtonDown("Pause") && !Paused)
         {
             Space.DispatchEvent(Events.Pause);
         }
-	}
+      else if(Input.GetButtonDown("Pause"))
+        {
+            Space.DispatchEvent(Events.UnPause);
+        }
+	  }
 
-    void OnUnPause(DefaultEvent eventdata)
+    void OnPause(DefaultEvent eventdata)
     {
-        Paused = false;
+        this.Paused = true;
+        this.MenuObject.gameObject.SetActive(true);
+    // test audio thing
+    }
+
+   void OnUnPause(DefaultEvent eventdata)
+    {
+        this.Paused = false;
+        this.MenuObject.gameObject.SetActive(false);
     }
 
 }
