07478a01672c64702315ee5a329ccb614a5aba70
Jetyl <jesse.lozano@digipen.edu>
2017-11-03 10:57:12 -0700

    the main menu buttons exist

--- Halfway Home/Assets/Scripts/MainMenu.cs
+++ Halfway Home/Assets/Scripts/MainMenu.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public int MainLevel = 1;
 
+    public Button ContinueButton;
+
     public GameObject ConfirmationPanel;
+    public GameObject ClearSavePanel;
 
 	// Use this for initialization
 	void Start ()
     {
+        SaveLoad.Load();
+
+        if (SaveLoad.GetSave(0) == null) //if a save exists, set that to the main game
+            ContinueButton.interactable = false;
 
         ConfirmationPanel.SetActive(false);
+        ClearSavePanel.SetActive(false);
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -25,14 +34,28 @@ public class MainMenu : MonoBehaviour
 
     public void NewGame()
     {
-        SaveLoad.Delete();
-        LoadLevel();
+        if (SaveLoad.GetSave(0) == null) //if a save exists, set that to the main game
+        {
+            ConfirmNew();
+        }
+        else
+        {
+            ClearSavePanel.SetActive(true);
+        }
+            
     }
     public void ContinueGame()
     {
         LoadLevel();
     }
 
+    public void ConfirmNew()
+    {
+
+        ClearSavePanel.SetActive(false);
+        SaveLoad.Delete();
+        LoadLevel();
+    }
 
     void LoadLevel()
     {
