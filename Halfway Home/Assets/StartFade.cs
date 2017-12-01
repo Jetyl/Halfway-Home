78abdb9f5b12867560d5909a3e0062ec9f3c573b
Jetyl <jesse.lozano@digipen.edu>
2017-12-01 11:45:47 -0800

    fixed the relase build crashing bug. also added fading between the menu and main scenes.

---
+++ Halfway Home/Assets/StartFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartFade : MonoBehaviour
+{
+
+    public Color StartColor = Color.black;
+
+    public Color EndColor;
+
+    public float FadeTime = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.GetComponent<Image>().color = StartColor;
+
+        gameObject.DispatchEvent(Events.Fade, new FadeEvent(EndColor, FadeTime));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
