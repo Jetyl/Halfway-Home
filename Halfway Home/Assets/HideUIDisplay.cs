b182bd05856f9f531d70755e38e9976e5a90c98f
Unknown <jalorn7@gmail.com>
2018-02-03 18:32:44 -0800

    Fixed UI Hiding

--- Halfway Home/Assets/HideUIDisplay.cs
+++ Halfway Home/Assets/HideUIDisplay.cs
@@ -8,6 +8,9 @@ public class HideUIDisplay : MonoBehaviour
 
     bool UIOn = true;
 
+  public GameObject[] IgnoredObjects;
+  private List<Graphic> activeUIElements = new List<Graphic>();
+
 	// Use this for initialization
 	void Start ()
     {
@@ -40,11 +43,11 @@ public class HideUIDisplay : MonoBehaviour
 
     void TurnUIOn()
     {
-        var gImages = gameObject.GetComponentsInChildren<Graphic>();
         UIOn = true;
-        foreach (var obj in gImages)
+        foreach (var obj in activeUIElements)
         {
             obj.enabled = true;
+            if(obj.GetComponent<Button>() != null) obj.GetComponent<Button>().enabled = true;
         }
         Space.DispatchEvent(Events.OpenUI);
     }
@@ -55,7 +58,17 @@ public class HideUIDisplay : MonoBehaviour
         UIOn = false;
         foreach (var obj in gImages)
         {
+          bool ignore = false;
+          foreach (GameObject g in IgnoredObjects)
+          {
+            if(g == obj.gameObject) ignore = true;
+          }
+          if (obj.gameObject.activeSelf & obj.enabled && !ignore)
+          {
             obj.enabled = false;
+            if(obj.GetComponent<Button>() != null) obj.GetComponent<Button>().enabled = false;
+            activeUIElements.Add(obj);
+          }
         }
         Space.DispatchEvent(Events.CloseUI);
     }
