4ec1328efbf64c869a2d4cb0b73bef1c06bae87d
Unknown <jalorn7@gmail.com>
2018-02-03 23:28:38 -0800

    Final Fix for UI Hiding
    Also changed the sprite on auto to a stop symbol while auto is enabled

---
+++ Halfway Home/Assets/Scripts/Utility/SpriteSwapper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class SpriteSwapper : MonoBehaviour
+{
+  public Sprite SwapSprite;
+  private Sprite StartSprite;
+  private bool Swapped = false;
+  private void Start()
+  {
+    StartSprite = GetComponent<Image>().sprite;
+  }
+  public void Swap()
+  {
+    if(Swapped)
+    {
+      ResetToOld();
+    }
+    else
+    {
+      SwapToNew();
+    }
+  }
+
+  void SwapToNew()
+  {
+    GetComponent<Image>().sprite = SwapSprite;
+    Swapped = true;
+  }
+
+  void ResetToOld()
+  {
+    GetComponent<Image>().sprite = StartSprite;
+    Swapped = true;
+  }
+}
