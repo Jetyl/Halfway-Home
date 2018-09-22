adec8e28f2f392cb9f5ff0ad77e069a99053efaa
Unknown <jalorn7@gmail.com>
2018-09-22 15:39:58 -0700

    Final Build Prep
    - Removed many unused files
    - Regenerated Sound Banks
    - Adjusted Warning Scene (new background and longer timer)
    - New Textless Logo

---
+++ Halfway Home/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,214 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID HEARTATTACK = 1078840671U;
+        static const AkUniqueID LPF1K_MUSIC_MAINMENU = 3373299806U;
+        static const AkUniqueID LPF4K_MUSIC_MAINMENU = 3370483183U;
+        static const AkUniqueID LPFOFF_MUSIC_MAINMENU = 3897416341U;
+        static const AkUniqueID MUSIC_LOOP_TEST = 4181321930U;
+        static const AkUniqueID PLAY_AMBIENCE_BIRDS = 285561943U;
+        static const AkUniqueID PLAY_AMBIENCE_BIRDS_LONG_FADE = 2370408065U;
+        static const AkUniqueID PLAY_AMBIENCE_CRICKETS_NIGHT = 889098130U;
+        static const AkUniqueID PLAY_AMBIENCE_CROWD = 2781780852U;
+        static const AkUniqueID PLAY_AMBIENCE_CROWD_WHISPERING = 54526557U;
+        static const AkUniqueID PLAY_AMBIENCE_FIREPLACE = 2929977910U;
+        static const AkUniqueID PLAY_AMBIENCE_FIREPLACE_BIRDS_DAY = 2897240348U;
+        static const AkUniqueID PLAY_AMBIENCE_FIREPLACE_CRICKETS_NIGHT = 792462442U;
+        static const AkUniqueID PLAY_AMBIENCE_FIREPLACE_FAR_BIRDS_DAY = 2634641494U;
+        static const AkUniqueID PLAY_AMBIENCE_FIREPLACE_FAR_CRICKETS_NIGHT = 2403255496U;
+        static const AkUniqueID PLAY_AMBIENCE_GARDEN_BIRDS_DAY = 1503005308U;
+        static const AkUniqueID PLAY_AMBIENCE_GARDEN_CRICKETS_NIGHT = 561449546U;
+        static const AkUniqueID PLAY_AMBIENCE_STORMY = 3646524025U;
+        static const AkUniqueID PLAY_AMBIENCE_YOUR_ROOM_FIREPLACE_BIRDS_DAY = 208869464U;
+        static const AkUniqueID PLAY_AMBIENCE_YOUR_ROOM_FIREPLACE_CRICKETS_NIGHT = 3202835318U;
+        static const AkUniqueID PLAY_MENUCLICK = 2576276897U;
+        static const AkUniqueID PLAY_MENUEXIT = 3169174387U;
+        static const AkUniqueID PLAY_MENUMOUSEOVER = 4008650974U;
+        static const AkUniqueID PLAY_MENUPAUSE = 3596481523U;
+        static const AkUniqueID PLAY_MENUUNPAUSE = 3101734072U;
+        static const AkUniqueID PLAY_MUSIC_ARTROOM = 1810713724U;
+        static const AkUniqueID PLAY_MUSIC_ARTROOM_MORNING = 444439289U;
+        static const AkUniqueID PLAY_MUSIC_ARTROOM_NO_INTRO = 2903575965U;
+        static const AkUniqueID PLAY_MUSIC_CAFEJAZZ = 240662438U;
+        static const AkUniqueID PLAY_MUSIC_CHARLOTTE_2 = 3914743577U;
+        static const AkUniqueID PLAY_MUSIC_CHARLOTTE_ELEGANT = 2039547829U;
+        static const AkUniqueID PLAY_MUSIC_COMMONS = 1005088852U;
+        static const AkUniqueID PLAY_MUSIC_CONTEMPLATIVE = 224129391U;
+        static const AkUniqueID PLAY_MUSIC_CREEPY_ATMO = 1372805254U;
+        static const AkUniqueID PLAY_MUSIC_CREEPY_ATMO_02 = 2374614039U;
+        static const AkUniqueID PLAY_MUSIC_DEJAVU = 1829857455U;
+        static const AkUniqueID PLAY_MUSIC_EDUARDO_LOVE = 3687385887U;
+        static const AkUniqueID PLAY_MUSIC_EDUARDO_LOVE_NO_INTRO = 601166520U;
+        static const AkUniqueID PLAY_MUSIC_EMOTIONAL_PIANO = 2723803496U;
+        static const AkUniqueID PLAY_MUSIC_FAKEIT = 2805407838U;
+        static const AkUniqueID PLAY_MUSIC_FAREWELL = 4092317258U;
+        static const AkUniqueID PLAY_MUSIC_GAME_DAY = 2946895563U;
+        static const AkUniqueID PLAY_MUSIC_GARDEN = 2702884089U;
+        static const AkUniqueID PLAY_MUSIC_HAPPY = 1020080944U;
+        static const AkUniqueID PLAY_MUSIC_LIBRARY = 3266052307U;
+        static const AkUniqueID PLAY_MUSIC_MAINMENU = 1566693136U;
+        static const AkUniqueID PLAY_MUSIC_PLACEHOLDER_MAIN = 1259498987U;
+        static const AkUniqueID PLAY_MUSIC_PLACEHOLDER_MAIN_FADEIN = 3941181009U;
+        static const AkUniqueID PLAY_MUSIC_SAD = 3637707742U;
+        static const AkUniqueID PLAY_MUSIC_STORE = 4068032989U;
+        static const AkUniqueID PLAY_MUSIC_STORMY_RELATIONSHIP = 3620952831U;
+        static const AkUniqueID PLAY_MUSIC_TENSION = 1712470628U;
+        static const AkUniqueID PLAY_MUSIC_TENSION_INTRO_01 = 1402380571U;
+        static const AkUniqueID PLAY_MUSIC_TENSION_INTRO_02 = 1402380568U;
+        static const AkUniqueID PLAY_MUSIC_TENSION_INTRO_03 = 1402380569U;
+        static const AkUniqueID PLAY_MUSIC_TENSION_INTRO_04 = 1402380574U;
+        static const AkUniqueID PLAY_MUSIC_UNFINISHED_BUSINESS = 3483334370U;
+        static const AkUniqueID PLAY_SFX_AMBIENT_HEARTBEAT = 197627707U;
+        static const AkUniqueID PLAY_SFX_BED_CREAK = 188792618U;
+        static const AkUniqueID PLAY_SFX_CAR_HORN = 1824880844U;
+        static const AkUniqueID PLAY_SFX_CREEPY_DOOR = 4285799733U;
+        static const AkUniqueID PLAY_SFX_DOOR_CREAK = 3759803571U;
+        static const AkUniqueID PLAY_SFX_FOLIAGE = 2942685983U;
+        static const AkUniqueID PLAY_SFX_HUMAN_FOOTSTEPS_APPROACHING = 2653434280U;
+        static const AkUniqueID PLAY_SFX_HUMAN_FOOTSTEPS_SOFT = 1787579972U;
+        static const AkUniqueID PLAY_SFX_HUMAN_GHOSTWHISPER = 2124921401U;
+        static const AkUniqueID PLAY_SFX_HUMAN_KNOCK = 2319499992U;
+        static const AkUniqueID PLAY_SFX_HUMAN_NOTEBOOK = 1091239431U;
+        static const AkUniqueID PLAY_SFX_MENU_AMBIENCE_SLIDER_SCROLL = 1234546846U;
+        static const AkUniqueID PLAY_SFX_MENU_EFFECTS_SLIDER_SCROLL = 1732677584U;
+        static const AkUniqueID PLAY_SFX_MENU_INTERFACE_SLIDER_SCROLL = 2186895855U;
+        static const AkUniqueID PLAY_SFX_MENU_MASTER_SLIDER_SCROLL = 2748965932U;
+        static const AkUniqueID PLAY_SFX_MENU_MUSIC_SLIDER_SCROLL = 174248703U;
+        static const AkUniqueID PLAY_SFX_MENU_SKIP = 1366361591U;
+        static const AkUniqueID PLAY_SFX_MENU_SLIDER_SCROLL = 3989971247U;
+        static const AkUniqueID PLAY_SFX_OBJECT_CAR_AWAY = 1700209709U;
+        static const AkUniqueID PLAY_SFX_STORE_BELL = 81198871U;
+        static const AkUniqueID PLAY_SFX_TIN_CANS = 2693693295U;
+        static const AkUniqueID PLAY_TEXTSCROLL = 319656260U;
+        static const AkUniqueID STOP_ALL = 452547817U;
+        static const AkUniqueID STOP_ALL_FAST = 3032100830U;
+        static const AkUniqueID STOP_AMBIENCE_ALL = 1720799084U;
+        static const AkUniqueID STOP_MUSIC_MAINMENU = 3152554426U;
+        static const AkUniqueID STOP_MUSIC_TENSION_INTRO_02 = 2883794594U;
+        static const AkUniqueID STOP_SFX_MENU_SKIP = 4059471133U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAME_STATE
+        {
+            static const AkUniqueID GROUP = 766723505U;
+
+            namespace STATE
+            {
+                static const AkUniqueID IN_GAME = 2967546505U;
+                static const AkUniqueID MAIN_MENU = 2005704188U;
+            } // namespace STATE
+        } // namespace GAME_STATE
+
+        namespace PAUSE
+        {
+            static const AkUniqueID GROUP = 3092587493U;
+
+            namespace STATE
+            {
+                static const AkUniqueID PAUSED = 319258907U;
+                static const AkUniqueID UNPAUSED = 1365518790U;
+            } // namespace STATE
+        } // namespace PAUSE
+
+        namespace TEXT_TYPE
+        {
+            static const AkUniqueID GROUP = 557060015U;
+
+            namespace STATE
+            {
+                static const AkUniqueID MONOLOGUE = 3368367120U;
+                static const AkUniqueID OTHER = 2376466361U;
+                static const AkUniqueID PLAYER = 1069431850U;
+            } // namespace STATE
+        } // namespace TEXT_TYPE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID AMBIENCE_LPF = 4047808182U;
+        static const AkUniqueID AMBIENCE_SLIDER = 129996979U;
+        static const AkUniqueID AMBIENCE_VOL = 4035164671U;
+        static const AkUniqueID EFFECTS_SLIDER = 2886374787U;
+        static const AkUniqueID MASTER_SLIDER = 2486326433U;
+        static const AkUniqueID MENU_MOUSE_HOVER = 3791118987U;
+        static const AkUniqueID MENU_SLIDER = 2902037724U;
+        static const AkUniqueID MUSIC_LPF = 2295956853U;
+        static const AkUniqueID MUSIC_SLIDER = 1127857866U;
+        static const AkUniqueID MUSIC_TENSION_STATE = 2789278851U;
+        static const AkUniqueID MUSIC_VOL = 2246609544U;
+        static const AkUniqueID TEXT_VOL = 1909810094U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID CREDITS = 2201105581U;
+        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MAINMENU = 3604647259U;
+        static const AkUniqueID MASTER = 4056684167U;
+        static const AkUniqueID ROOM_ARTROOM = 2842449619U;
+        static const AkUniqueID ROOM_COMMONS = 3114788647U;
+        static const AkUniqueID ROOM_GARDEN = 1993474908U;
+        static const AkUniqueID ROOM_KITCHEN = 265238677U;
+        static const AkUniqueID ROOM_LIBRARY = 3564131344U;
+        static const AkUniqueID ROOM_STORE = 2623393530U;
+        static const AkUniqueID ROOM_YOURROOM = 474505651U;
+        static const AkUniqueID STORY_ALONE_IN_A_DEAD_MANS_ROOM = 609072037U;
+        static const AkUniqueID STORY_AUTHORITYS_APPROVAL = 3786498133U;
+        static const AkUniqueID STORY_BIG_HAPPY_FAMILY = 4068719739U;
+        static const AkUniqueID STORY_CHARLOTTE = 2790364613U;
+        static const AkUniqueID STORY_CLOSED_WOUND = 2826146263U;
+        static const AkUniqueID STORY_COLD_SKY_HIDEAWAY = 2982646672U;
+        static const AkUniqueID STORY_COURTSIDE = 817531469U;
+        static const AkUniqueID STORY_DYE_JOB = 1127752803U;
+        static const AkUniqueID STORY_ENDING_DAYS = 2369017504U;
+        static const AkUniqueID STORY_FAREWELL_MEAL = 1928416423U;
+        static const AkUniqueID STORY_KNOCK_ON_THE_MORNING_DOOR = 4021864313U;
+        static const AkUniqueID STORY_MEETING_CHARLOTTE = 491544915U;
+        static const AkUniqueID STORY_MEETING_EDUARDO = 65122749U;
+        static const AkUniqueID STORY_MEMORY = 1576038406U;
+        static const AkUniqueID STORY_MORNING_MISERY = 4020804685U;
+        static const AkUniqueID STORY_OPEN_LETTER = 3653532294U;
+        static const AkUniqueID STORY_POST_BREAKDOWN = 2248810909U;
+        static const AkUniqueID STORY_RECOVER_RESTART_REQUIRED = 3861464175U;
+        static const AkUniqueID STORY_WELCOME_DINNER = 2139259380U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE_BUS = 4202237879U;
+        static const AkUniqueID EFFECTS_BUS = 1604779591U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MENU_BUS = 3931159050U;
+        static const AkUniqueID MENU_HOVER_BUS = 238729384U;
+        static const AkUniqueID MUSIC_BUS = 3127962312U;
+        static const AkUniqueID TENSION_MUSIC_BUS = 315519858U;
+        static const AkUniqueID TENSION_STEM_01_BUS = 1759925539U;
+        static const AkUniqueID TENSION_STEM_02_BUS = 598114000U;
+        static const AkUniqueID TENSION_STEM_03_BUS = 3079149437U;
+        static const AkUniqueID TENSION_STEM_04_BUS = 2721585490U;
+        static const AkUniqueID TENSION_STEM_05_BUS = 1482909111U;
+        static const AkUniqueID TENSION_STEM_06_BUS = 1267988132U;
+        static const AkUniqueID TENSION_STEM_07_BUS = 3212139665U;
+        static const AkUniqueID TEXT_SCROLL_BUS = 1929927065U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
